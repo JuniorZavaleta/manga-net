f649192cae26bdd48418132c386aaa5a52a8f1e4
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2018-10-12 00:22:16 -0500

    separar en dos las paginas del dataset

--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,48 @@
-from skimage import io, transform
-from torchvision import transforms
+import os
 
-class CustomCrop(object):
+import redis
 
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
+from skimage import io
 
-    def __call__(self, sample):
-        image = sample['image']
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+list_dir = os.listdir('data/AisazuNihaIrarenai')
+mangaId = 3
 
 
-transformations = transforms.Compose([
-    CustomCrop)
+def split(filename):
+    """TODO"""
+
+
+# Se ignoran las primeras dos páginas 000 y 001 porque no suelen aportar
+for i in range(2, 16):
+    for filename in list_dir:
+        if filename.split(".")[0] == str(i).zfill(3):
+            img = io.imread('data/AisazuNihaIrarenai/'+filename, as_gray=True)
+
+            height, width = img.shape
+            width_cutoff = width // 2
+            page_left = img[:, :width_cutoff]
+            page_right = img[:, width_cutoff:]
+
+            right = (i - 2) * 2 + 1
+            left = right + 1
+            io.imsave("manga/{}/page{}.jpg".format(mangaId, right), page_right)
+            io.imsave("manga/{}/page{}.jpg".format(mangaId, left), page_left)
+
+
+# from skimage import io, transform
+# from torchvision import transforms
+#
+# class CustomCrop(object):
+#
+#     def __init__(self, output_size):
+#         assert isinstance(output_size, (int, tuple))
+#         self.output_size = output_size
+#
+#     def __call__(self, sample):
+#         image = sample['image']
+#
+#
+# transformations = transforms.Compose([
+#     CustomCrop])
